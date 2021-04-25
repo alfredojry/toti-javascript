991e36b9fc2e620c35f9a7b1477cfd0105db6ef8
alfredo <yoelvisjimenezr@gmail.com>
2021-04-25 15:53:27 -0500

    exercicio 05 - api fetch

--- README.md
+++ README.md
@@ -6,3 +6,5 @@
 [Exercício 03 - JavaScript](https://alfredojry.github.io/toti-javascript/exercicio-3)
 
 [Exercício 04 - JavaScript](https://alfredojry.github.io/toti-javascript/exercicio-4)
+
+[Exercício 05 - JavaScript](https://alfredojry.github.io/toti-javascript/exercicio-5)
---
+++ exercicio-5/script.js
@@ -0,0 +1,43 @@
+// carregando os items UF do select
+window.addEventListener('DOMContentLoaded', function() {
+    fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados/')
+        .then(resp => resp.json())
+        .then(data => {
+            let dataSorted = data.sort((a, b) => a.nome.localeCompare(b.nome));
+            let select = document.querySelector('select');
+            for (let item of dataSorted) {
+                let option = document.createElement('option');
+                option.value = item.sigla;
+                option.textContent = item.nome;
+                select.appendChild(option);
+            }
+        })
+        .catch(error => {console.log(error);});
+});
+
+// carregando a informação do estado
+let select = document.querySelector('select');
+select.addEventListener('change', function (event) {
+    let sigla = event.target.value;
+    let head = document.getElementById('uf-head');
+    let ul = document.querySelector('ul');
+    head.innerHTML = '<p>Por favor, espere...</p>';
+    ul.innerHTML = '';
+    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${sigla}/municipios`)
+        .then(resp => resp.json())
+        .then(data => {
+            let uf = data[0].microrregiao.mesorregiao.UF.nome;
+            head.innerHTML = '';
+            let h2 = document.createElement('h2');
+            let p = document.createElement('p');
+            h2.textContent = `Nome: ${uf}`;
+            p.textContent = `Número de municípios: ${data.length}`;
+            head.appendChild(h2);
+            head.appendChild(p);
+            for (let item of data) {
+                let li = document.createElement('li');
+                li.textContent = item.nome;
+                ul.appendChild(li);
+            }
+        })
+});
